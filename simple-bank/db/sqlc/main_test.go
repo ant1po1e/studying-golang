2711b58cc5d1e3a3cc254d23e2dd2d6c0d189a0c
Yudha Pratama <symamaxim@gmail.com>
2023-08-30 10:40:14 +0700

    added simple bank

---
+++ simple-bank/db/sqlc/main_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to DB:", err)
+	}
+
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
